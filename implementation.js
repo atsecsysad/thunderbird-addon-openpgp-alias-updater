a2574bcd4567a6cf18d21877754672bc285f5cd0
pi-2007 <70692117+pi-2007@users.noreply.github.com>
2024-08-09 17:29:52 +0200

    updated move file for compatibility with TB 128.X
    thanks to TB's addon reviewer

--- implementation.js
+++ implementation.js
@@ -1,13 +1,11 @@
-var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
-var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-
+/* global ExtensionCommon, IOUtils, PathUtils */
 
 var moveFile = class extends ExtensionCommon.ExtensionAPI {
   getAPI(context) {
     return {
       moveFile: {
         async moveFile(sourcePath, json_filename) {
-        	IOUtils.move(sourcePath, PathUtils.join(PathUtils.profileDir, json_filename));
+          await IOUtils.move(sourcePath, PathUtils.join(PathUtils.profileDir, json_filename));
         }
       }
     }
